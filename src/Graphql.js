8529af56bcd6bbf663927a7a803a61c9fbcda8f2
ANELL19 <lizcuevas68@gmail.com>
2022-03-05 14:09:58 -0600

    prueba deploy front diagnostico

--- src/Graphql.js
+++ src/Graphql.js
@@ -5,7 +5,8 @@ import { createUploadLink } from 'apollo-upload-client';
 
 //  const API_URL = 'https://backenddiagnostico035.herokuapp.com/graphql'; //URL
 
-const API_URL = 'http://localhost:4000/graphql'; //URL
+const API_URL = 'https://backdiagnosticoprueba.herokuapp.com/graphql'
+// const API_URL = 'http://localhost:4000/graphql'; //URL
 
 const httpLink = createUploadLink({
     uri: `${API_URL}`
