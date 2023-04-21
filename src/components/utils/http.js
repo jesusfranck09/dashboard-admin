0faf3361500705aecfb79a78d38a689f92cf14f3
jesusfranck09 <d93409@gmail.com>
2023-04-20 19:13:24 -0600

    update

--- src/components/utils/http.js
+++ src/components/utils/http.js
@@ -1,7 +1,7 @@
 
-// const API =  'https://backenddiagnostico035.herokuapp.com/graphql'
+const API =  'https://backenddiagnostico035.herokuapp.com/graphql'
 // const API = 'https://backdiagnosticoprueba.herokuapp.com/graphql'
- const API =  'http://localhost:4000/graphql'
+//  const API =  'http://localhost:4000/graphql'
 // 
 
 export {
