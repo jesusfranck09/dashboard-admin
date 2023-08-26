e3ffe0061da7f339080216220064ae9901b95c38
jesusfranck09 <d93409@gmail.com>
2023-08-25 21:45:36 -0600

    update

--- src/components/ResGenerales/reportRP/reportRPGE.jsx
+++ src/components/ResGenerales/reportRP/reportRPGE.jsx
@@ -2297,7 +2297,7 @@ class ReportRPGE extends Component {
                                                 <br></br>  
                                                 <br></br>     
                                                 <center>
-                                                    <p className="textabla2"><strong>GUÍA DE REFERENCIA III
+                                                    <p className="textabla2"><strong>GUÍA DE REFERENCIA II
                                                     CUESTIONARIO PARA IDENTIFICAR LOS FACTORES DE RIESGO PSICOSOCIAL Y EVALUAR EL ENTORNO ORGANIZACIONAL EN
                                                     LOS CENTROS DE TRABAJO</strong></p> 
                                                 </center>
